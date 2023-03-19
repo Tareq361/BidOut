bad9d88221cdddfa8e6ea5e7ffb12d2f70e07195
Tareq361 <mtareq361@gmail.com>
2023-03-19 23:44:22 +0600

    update for asgi

--- GrowexoAuction/routing.py
+++ GrowexoAuction/routing.py
@@ -1,13 +1,16 @@
 import os
 
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 import Auction.routing
 
-asgi_app = get_asgi_application()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GrowexoAuction.settings')
+django.setup()
+asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
 
     {
